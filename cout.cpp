41e22c4263450c8f49e6cb4d7cbd8e1d7cbabb42
weakson <950823eason@gmail.com>
2025-01-11 22:52:42 +0800

    hello world

---
+++ cout.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  cout << 1 << '\n'; // 輸出 1 之後換行 
+  cout << 'a' << '\n'; // 輸出 a 之後換行
+  cout << 3.14 << '\n'; // 輸出 3.14 之後換行
+  cout << "Hello world\n"; // 輸出 Hello wolrd 之後換行
+
+  int age = 19;
+  cout << "Your age is " << age << ".\n"; // 輸出 Your age is 19. 然後換行
+
+}
