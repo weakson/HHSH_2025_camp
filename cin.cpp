4335ece879ca5934c806bf7f39395970a5f345ee
weakson <950823eason@gmail.com>
2025-01-11 23:32:42 +0800

    edit tabstop

--- cin.cpp
+++ cin.cpp
@@ -3,4 +3,15 @@ using namespace std;
 
 int main(){
 
+    int a, b;
+    char c;
+    double d;
+
+    cin >> a >> b >> c >> d;
+
+    cout << "a = " << a << '\n';
+    cout << "b = " << b << '\n';
+    cout << "c = " << c << '\n';
+    cout << "d = " << d << '\n';
+
 }
--- cout.cpp
+++ cout.cpp
@@ -3,21 +3,21 @@ using namespace std;
 
 int main(){
 
-  cout << 1 << '\n'; // 輸出 1 之後換行 
-  cout << 'a' << '\n'; // 輸出 a 之後換行
-  cout << 3.14 << '\n'; // 輸出 3.14 之後換行
-  cout << "Hello world\n"; // 輸出 Hello wolrd 之後換行
+    cout << 1 << '\n'; // 輸出 1 之後換行 
+    cout << 'a' << '\n'; // 輸出 a 之後換行
+    cout << 3.14 << '\n'; // 輸出 3.14 之後換行
+    cout << "Hello world\n"; // 輸出 Hello wolrd 之後換行
 
-  int age = 19;
-  cout << "Your age is " << age << ".\n"; // 輸出 Your age is 19. 然後換行
+    int age = 19;
+    cout << "Your age is " << age << ".\n"; // 輸出 Your age is 19. 然後換行
 
-  /*
-   * output:
-   * 1
-   * a
-   * 3.14
-   * Hello world
-   * Your age is 19.
-  */
+    /*
+    * output:
+    * 1
+    * a
+    * 3.14
+    * Hello world
+    * Your age is 19.
+    */
 
 }
