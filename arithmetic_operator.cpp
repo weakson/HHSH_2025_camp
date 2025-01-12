a9c6119b8ba32c1853d01bccfcbd282596c3dd67
weakson <950823eason@gmail.com>
2025-01-12 09:44:48 +0800

    add arithmetic_operator.cpp

---
+++ arithmetic_operator.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a, b = 10;
+
+    a = 10 + 20;             // a = 30
+    a = 50 - 25;             // a = 25
+    a = 2 * 5;               // a = 10
+    a = 10 / 3;              // a = 3
+    a = 22 % 7;              // a = 1
+    a = (((5 + 3) * 4) + 3); // a = 35
+    a = 2 + 4 * 3 / 2;       // a = 8
+
+    a = 10;
+    a = a + b; // a = 20
+    a = a * b; // a = 200
+
+}
