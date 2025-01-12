aacbfa9d4b3a58a239c737826752a63efa252c3c
weakson <950823eason@gmail.com>
2025-01-12 16:54:00 +0800

    add comparison_operator.cpp

---
+++ comparison_operator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 10, b = 5;
+
+    cout << (a < b) << '\n';  // 0
+    cout << (a <= b) << '\n'; // 0
+    cout << (a > b) << '\n';  // 1
+    cout << (a >= b) << '\n'; // 1
+    cout << (a == b) << '\n'; // 0
+    cout << (a != b) << '\n'; // 1
+
+}
