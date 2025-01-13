b3bd54a5d7bc5305516b1f87c397a3f94b608d8d
weakson <950823eason@gmail.com>
2025-01-13 23:15:29 +0800

    add zj_d067.cpp

---
+++ zj_d067.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    if (!(n % 400 != 0 && n % 100 == 0) && n % 4 == 0){
+        cout << "a leap year\n";
+    }
+    else{
+        cout << "a normal year\n";
+    }
+
+}
