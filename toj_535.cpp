4a69282b5e3e525ee05d499c1c58b621c6b20adc
weakson <950823eason@gmail.com>
2025-01-13 22:55:55 +0800

    add toj_535.cpp

---
+++ toj_535.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int s;
+    cin >> s;
+
+    if (s <= 59) cout << "F\n";
+    else if (s <= 69) cout << "D\n";
+    else if (s <= 79) cout << "C\n";
+    else if (s <= 89) cout << "B\n";
+    else if (s <= 99) cout << "A\n";
+    else cout << "S\n";
+
+}
