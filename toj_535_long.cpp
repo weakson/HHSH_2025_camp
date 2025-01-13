53831e0024759028088e8ba6243e3b016108977b
weakson <950823eason@gmail.com>
2025-01-13 23:03:23 +0800

    add toj_535_long.cpp

---
+++ toj_535_long.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int s;
+    cin >> s;
+
+    if (s >= 0 && s <= 59) cout << "F\n";
+    else if (s >= 60 && s <= 69) cout << "D\n";
+    else if (s >= 70 && s <= 79) cout << "C\n";
+    else if (s >= 80 && s <= 89) cout << "B\n";
+    else if (s >= 90 && s <= 99) cout << "A\n";
+    else cout << "S\n";
+
+}
