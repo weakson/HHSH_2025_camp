55c0fbed5f4d6d6f909bb661c9bc3a6115effab4
weakson <950823eason@gmail.com>
2025-01-14 10:16:43 +0800

    edit toj_535

--- toj_535.cpp
+++ toj_535.cpp
@@ -6,11 +6,23 @@ int main(){
     int s;
     cin >> s;
 
-    if (s <= 59) cout << "F\n";
-    else if (s <= 69) cout << "D\n";
-    else if (s <= 79) cout << "C\n";
-    else if (s <= 89) cout << "B\n";
-    else if (s <= 99) cout << "A\n";
-    else cout << "S\n";
+    if (s <= 59){
+        cout << "F\n";
+    }
+    else if (s <= 69){
+        cout << "D\n";
+    }
+    else if (s <= 79){
+        cout << "C\n";
+    }
+    else if (s <= 89){
+        cout << "B\n";
+    }
+    else if (s <= 99){
+        cout << "A\n";
+    }
+    else{
+        cout << "S\n";
+    }
 
 }
--- toj_535_long.cpp
+++ toj_535_long.cpp
@@ -6,11 +6,23 @@ int main(){
     int s;
     cin >> s;
 
-    if (s >= 0 && s <= 59) cout << "F\n";
-    else if (s >= 60 && s <= 69) cout << "D\n";
-    else if (s >= 70 && s <= 79) cout << "C\n";
-    else if (s >= 80 && s <= 89) cout << "B\n";
-    else if (s >= 90 && s <= 99) cout << "A\n";
-    else cout << "S\n";
+    if (s >= 0 && s <= 59){
+        cout << "F\n";
+    }
+    else if (s >= 60 && s <= 69){
+        cout << "D\n";
+    }
+    else if (s >= 70 && s <= 79){
+        cout << "C\n";
+    }
+    else if (s >= 80 && s <= 89){
+        cout << "B\n";
+    }
+    else if (s >= 90 && s <= 99){
+        cout << "A\n";
+    }
+    else{
+        cout << "S\n";
+    }
 
 }
