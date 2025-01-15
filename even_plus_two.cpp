9bdb916717515542d455166c027650719abb0e80
weakson <950823eason@gmail.com>
2025-01-15 11:08:10 +0800

    add even_plus_two.cpp

---
+++ even_plus_two.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int n;
+    cin >> n;
+
+    int i = 2;
+    while (i <= n){
+        cout << i << ' ';
+        i += 2;
+    }
+    cout << '\n';
+
+}
