a3e96c9166aaa6dc673cf49fda4fc2e282dcfd48
weakson <950823eason@gmail.com>
2025-01-15 18:31:54 +0800

    edit abc144_b.cpp

--- abc144_b.cpp
+++ abc144_b.cpp
@@ -6,14 +6,17 @@ int main(){
     int N;
     cin >> N;
 
-    bool is_found = false;
+    bool is_found = false; // 判斷有沒有找到 b，一開始假設沒有（false）
 
-    for (int i = 1; i <= 9; i++){
-        if (N % i == 0 && N / i <= 9){
+    for (int a = 1; a <= 9; a++){
+        if (N % a == 0 && N / a <= 9){
+            // 找到 b 了，將 is_found 設為 true
             is_found = true;
         }
     }
 
+    // is_found 為 true 表示有找到 b
+    // 反之表示沒找到
     if (is_found) cout << "Yes\n";
     else cout << "No\n";
 
