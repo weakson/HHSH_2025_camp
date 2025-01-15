775ce0103fcd4035b42edbf6fc925dc298f9ca63
weakson <950823eason@gmail.com>
2025-01-15 11:04:45 +0800

    add odd_mod.cpp

---
+++ odd_mod.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int n;
+    cin >> n;
+
+    int i = 2;
+    while (i <= n){
+        if (i % 2 == 0) cout << i << ' ';
+        i++;
+    }
+    cout << '\n';
+
+}
