74258c2e8f9380086f2b0d00fa5b1b686f10915a
weakson <950823eason@gmail.com>
2025-01-20 00:18:31 +0800

    add getline_problem.cpp

---
+++ getline_problem.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    string s;
+
+    cin >> n;
+
+    getline (cin, s);
+
+    cout << "n = " << n << '\n';
+    cout << "s = " << s << '\n';
+
+}
