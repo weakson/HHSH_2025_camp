c0daff4c439eade1f4f64c3daf448f7c84294572
weakson <950823eason@gmail.com>
2025-01-19 23:43:42 +0800

    add abc217_a.cpp

---
+++ abc217_a.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string S, T;
+    cin >> S >> T;
+
+    if (S < T){
+        cout << "Yes\n";
+    }
+    else{
+        cout << "No\n";
+    }
+
+}
