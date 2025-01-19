3ef640c25612b39e545fc48c0a9eefeb58df4065
weakson <950823eason@gmail.com>
2025-01-19 23:32:53 +0800

    add abc215_a.cpp

---
+++ abc215_a.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string s;
+    cin >> s;
+
+    if (s == "Hello,World!"){
+        cout << "AC\n";
+    }
+    else{
+        cout << "WA\n";
+    }
+
+}
