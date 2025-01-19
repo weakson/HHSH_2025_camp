d10e51ba4f819a871c37e69aba41d74f30712a2a
weakson <950823eason@gmail.com>
2025-01-19 22:08:30 +0800

    add abc095_a.cpp

---
+++ abc095_a.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string s;
+    cin >> s;
+
+    int cnt = 0;
+    if (s[0] == 'o'){
+        cnt++;
+    }
+    if (s[1] == 'o'){
+        cnt++;
+    }
+    if (s[2] == 'o'){
+        cnt++;
+    }
+
+    cout << 700 + cnt * 100 << '\n';
+
+}
