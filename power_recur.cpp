1b3773e851851836b5d000d26cda0b0ae2e89505
weakson <950823eason@gmail.com>
2025-02-02 17:08:06 +0800

    add power_recur.cpp

---
+++ power_recur.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int f (int a, int n){
+
+    if (n == 1){
+        return a;
+    }
+
+    return a * f (a, n - 1);
+
+}
+
+int main(){
+
+    int a, n;
+    cin >> a >> n;
+
+    cout << f (a, n) << '\n';
+
+}
